7cc955fbefb50ded8da3fb54df72f4fd253d746e
Gaurav Dhingra <grvdhingra1999@gmail.com>
2020-04-10 02:15:18 +0530

    Homepage Structure

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/data/data.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.account_circle,
+            size: 30,
+          ),
+          onPressed: () {},
+        ),
+        title: Text('Skippp The Meals'),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () {},
+            child: Text(
+              'Cart (${currentUser.cart.length})',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(
+                  vertical: 15,
+                ),
+                fillColor: Colors.white,
+                // fillColor: Colors.blue,
+                // filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide(
+                    width: 0.8,
+                  ),
+                ),
+                hintText: 'Search Food or Restaurants',
+                prefixIcon: Icon(
+                  Icons.search,
+                  size: 30,
+                ),
+                suffixIcon:
+                    IconButton(icon: Icon(Icons.clear), onPressed: () {}),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
